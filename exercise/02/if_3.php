173d38f6ae76aa5c7f7196353c1fd84b01444d88
akinobu <siguiente.football.373@gmail.com>
2021-09-12 22:19:12 +0900

    配列と連想配列の課題完了

---
+++ exercise/02/if_3.php
@@ -0,0 +1,15 @@
+<?php
+
+
+// $score = 70;
+// $score = 50;
+$score = 10;
+
+
+if($score>=60){
+    echo '合格です';
+}elseif($score<60 && $score>=30){
+    echo '追試です';
+}else{
+    echo'不合格です';
+}
---
+++ exercise/02/if_4.php
@@ -0,0 +1,19 @@
+<?php
+
+// $score_math =80;
+// $score_english =75;
+
+// $score_math = 50;
+// $score_english = 24;
+
+$score_math = 15;
+$score_english = 24;
+
+if($score_math>=60 && $score_english>=60){
+    echo '合格';
+}elseif($score_math>=50 || $score_english>=50){
+    echo '再試験';
+}else{
+    echo'不合格';
+}
+
